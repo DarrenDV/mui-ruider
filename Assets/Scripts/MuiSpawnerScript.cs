e1a47bb4ce888820e5cd8faaa9e6f619d18b1fb6
Omega_The_III <jeroen.officier@acda.nl>
2022-02-18 13:10:11 +0100

    Mui creator made

--- Assets/Scripts/MuiSpawnerScript.cs
+++ Assets/Scripts/MuiSpawnerScript.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class MuiSpawnerScript : MonoBehaviour
 {
     [SerializeField] private GameObject[] muien = new GameObject[3];
+    [SerializeField] private GameObject muiHolder;
     public float worldLength;
     public int maxWorldZ;
-    
-    void Start()
+    private void Start()
+    {
+        CreateMuien();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            DeleteMuien();
+        }
+    }
+    private void CreateMuien()
     {
         for (int i = 0; i < worldLength; i++)
         {
-            int randomMui = Random.Range(0,3);
+            int randomMui = Random.Range(0, 3);
             float randomXOffset = Random.Range(-2.0f, 3.0f);
             float randomZOffset = Random.Range(-maxWorldZ, maxWorldZ + 1);
-            Instantiate(muien[randomMui], new Vector3((i * 10) + randomXOffset, 0.5f, randomZOffset), Quaternion.identity);
+            Instantiate(muien[randomMui], new Vector3((i * 10) + randomXOffset, 0.5f, randomZOffset), Quaternion.identity, muiHolder.transform);
         }
     }
-    private void FixedUpdate()
+    private void DeleteMuien()
     {
-        
+        foreach(Transform child in muiHolder.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        CreateMuien();
     }
 }
